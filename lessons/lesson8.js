502674f0bf5680fc6b9c8dd28a90444f7f72a719
abraxas93 <savchyk.a@gmail.com>
2017-03-09 13:27:15 +0200

    added classes lesson

--- lessons/lesson8.js
+++ lessons/lesson8.js
@@ -1,4 +1,4 @@
-console.log('Lesson 7: Classes');
+console.log('Lesson 8: Classes');
 
 class Task {
     constructor(title = Task.getDefaultTitle()) {
@@ -8,10 +8,12 @@ class Task {
         console.log(`Task created with title:${this._title}`);
     }
 
+    /* Property getter */
     get done() {
         return this._done === true ? 'Task is done' : 'Task is not done';
     }
 
+    /* Property setter */
     set done(value) {
         if (value !== undefined && typeof value === 'boolean') {
             this._done = value;
@@ -20,21 +22,26 @@ class Task {
         }
     }
 
+    /* Class method */
     complete() {
         this.done = true;
         console.log(`Task ${this._title} is done`);
     }
-
+    /* Static class method */
     static getDefaultTitle() {
         return 'Defatul title';
     }
 }
-
+/* Static class property */
 Task.count = 0;
 
 let task1 = new Task('Custom task');
+let task2 = new Task();
 
 console.log(task1._done, task1.done);
 
 task1.complete();
 console.log(task1._done, task1.done);
+console.log(task1);
+console.log(task2);
+console.log(Task.count);
